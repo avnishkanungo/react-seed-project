e93be5b53fe259135ad6a1f441bc68e8943934fc
Avnish Kanungo <avnishkanungo@Avnishs-MacBook-Pro.local>
2019-02-27 16:59:20 +0530

    flux architecture working

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var list = require("./components/list.jsx");
+
+ReactDOM.render(<list />, document.getElementById("ingredient"));
